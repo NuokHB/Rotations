2fd1a59c7ab7eb9ef72576313a24db15ee564fc1
NuokHB <rsd1990@gmail.com>
2022-05-16 12:19:25 +0100

    Updated Ids

--- Mage/Frost_Wrath.lua
+++ Mage/Frost_Wrath.lua
@@ -3,71 +3,71 @@ local ni = ...
 
 local spells = {
 --General
- ArcaneAffinity = {id = 28877, name = ni.spell.info(28877)},
- ArcaneTorrent = {id = 28730, name = ni.spell.info(28730)},
- AutoAttack = {id = 6603, name = ni.spell.info(6603)},
- Disenchant = {id = 13262, name = ni.spell.info(13262)},
- Dodge = {id = 81, name = ni.spell.info(81)},
- Enchanting = {id = 7411, name = ni.spell.info(7411)},
- MagicResistance = {id = 822, name = ni.spell.info(822)},
- Shoot = {id = 5019, name = ni.spell.info(5019)},
+ArcaneAffinity = {id = 28877, name = ni.spell.info(28877)},
+ArcaneTorrent = {id = 28730, name = ni.spell.info(28730)},
+AutoAttack = {id = 6603, name = ni.spell.info(6603)},
+Disenchant = {id = 13262, name = ni.spell.info(13262)},
+Dodge = {id = 81, name = ni.spell.info(81)},
+Enchanting = {id = 7411, name = ni.spell.info(7411)},
+MagicResistance = {id = 822, name = ni.spell.info(822)},
+Shoot = {id = 5019, name = ni.spell.info(5019)},
 --Arcane
- AmplifyMagic = {id = 33946, name = ni.spell.info(33946)},
- ArcaneBlast = {id = 42894, name = ni.spell.info(42894)},
- ArcaneBrilliance = {id = 27127, name = ni.spell.info(27127)},
- ArcaneExplosion = {id = 27082, name = ni.spell.info(27082)},
- ArcaneIntellect = {id = 27126, name = ni.spell.info(27126)},
- ArcaneMissiles = {id = 42843, name = ni.spell.info(42843)},
- Blink = {id = 1953, name = ni.spell.info(1953)},
- ConjureFood = {id = 33717, name = ni.spell.info(33717)},
- ConjureManaGem = {id = 27101, name = ni.spell.info(27101)},
- ConjureRefreshment = {id = 42955, name = ni.spell.info(42955)},
- ConjureWater = {id = 27090, name = ni.spell.info(27090)},
- Counterspell = {id = 2139, name = ni.spell.info(2139)},
- DampenMagic = {id = 33944, name = ni.spell.info(33944)},
- Evocation = {id = 12051, name = ni.spell.info(12051)},
- FocusMagic = {id = 54646, name = ni.spell.info(54646)},
- Invisibility = {id = 66, name = ni.spell.info(66)},
- MageArmor = {id = 43023, name = ni.spell.info(43023)},
- ManaShield = {id = 43019, name = ni.spell.info(43019)},
- Polymorph = {id = 12826, name = ni.spell.info(12826)},
- PortalDalaran = {id = 53142, name = ni.spell.info(53142)},
- PortalShattrath = {id = 35717, name = ni.spell.info(35717)},
- PortalSilvermoon = {id = 32267, name = ni.spell.info(32267)},
- PortalUndercity = {id = 11418, name = ni.spell.info(11418)},
- RemoveCurse = {id = 475, name = ni.spell.info(475)},
- RitualofRefreshment = {id = 43987, name = ni.spell.info(43987)},
- SlowFall = {id = 130, name = ni.spell.info(130)},
- Spellsteal = {id = 30449, name = ni.spell.info(30449)},
- TeleportDalaran = {id = 53140, name = ni.spell.info(53140)},
- TeleportShattrath = {id = 35715, name = ni.spell.info(35715)},
- TeleportSilvermoon = {id = 32272, name = ni.spell.info(32272)},
- TeleportUndercity = {id = 3563, name = ni.spell.info(3563)},
+AmplifyMagic = {id = 43017, name = ni.spell.info(43017)},
+ArcaneBlast = {id = 42896, name = ni.spell.info(42896)},
+ArcaneBrilliance = {id = 27127, name = ni.spell.info(27127)},
+ArcaneExplosion = {id = 42920, name = ni.spell.info(42920)},
+ArcaneIntellect = {id = 27126, name = ni.spell.info(27126)},
+ArcaneMissiles = {id = 42843, name = ni.spell.info(42843)},
+Blink = {id = 1953, name = ni.spell.info(1953)},
+ConjureFood = {id = 33717, name = ni.spell.info(33717)},
+ConjureManaGem = {id = 42985, name = ni.spell.info(42985)},
+ConjureRefreshment = {id = 42955, name = ni.spell.info(42955)},
+ConjureWater = {id = 27090, name = ni.spell.info(27090)},
+Counterspell = {id = 2139, name = ni.spell.info(2139)},
+DampenMagic = {id = 43015, name = ni.spell.info(43015)},
+Evocation = {id = 12051, name = ni.spell.info(12051)},
+FocusMagic = {id = 54646, name = ni.spell.info(54646)},
+Invisibility = {id = 66, name = ni.spell.info(66)},
+MageArmor = {id = 43023, name = ni.spell.info(43023)},
+ManaShield = {id = 43019, name = ni.spell.info(43019)},
+Polymorph = {id = 12826, name = ni.spell.info(12826)},
+PortalDalaran = {id = 53142, name = ni.spell.info(53142)},
+PortalShattrath = {id = 35717, name = ni.spell.info(35717)},
+PortalSilvermoon = {id = 32267, name = ni.spell.info(32267)},
+PortalUndercity = {id = 11418, name = ni.spell.info(11418)},
+RemoveCurse = {id = 475, name = ni.spell.info(475)},
+RitualofRefreshment = {id = 43987, name = ni.spell.info(43987)},
+SlowFall = {id = 130, name = ni.spell.info(130)},
+Spellsteal = {id = 30449, name = ni.spell.info(30449)},
+TeleportDalaran = {id = 53140, name = ni.spell.info(53140)},
+TeleportShattrath = {id = 35715, name = ni.spell.info(35715)},
+TeleportSilvermoon = {id = 32272, name = ni.spell.info(32272)},
+TeleportUndercity = {id = 3563, name = ni.spell.info(3563)},
 --Fire
- FireBlast = {id = 42872, name = ni.spell.info(42872)},
- FireWard = {id = 27128, name = ni.spell.info(27128)},
- Fireball = {id = 42832, name = ni.spell.info(42832)},
- Flamestrike = {id = 42925, name = ni.spell.info(42925)},
- FrostfireBolt = {id = 44614, name = ni.spell.info(44614)},
- MoltenArmor = {id = 43045, name = ni.spell.info(43045)},
- Scorch = {id = 42858, name = ni.spell.info(42858)},
+FireBlast = {id = 42872, name = ni.spell.info(42872)},
+FireWard = {id = 43010, name = ni.spell.info(43010)},
+Fireball = {id = 42833, name = ni.spell.info(42833)},
+Flamestrike = {id = 42925, name = ni.spell.info(42925)},
+FrostfireBolt = {id = 44614, name = ni.spell.info(44614)},
+MoltenArmor = {id = 43045, name = ni.spell.info(43045)},
+Scorch = {id = 42859, name = ni.spell.info(42859)},
 --Frost
- Blizzard = {id = 42939, name = ni.spell.info(42939)},
- ColdSnap = {id = 11958, name = ni.spell.info(11958)},
- ConeofCold = {id = 42930, name = ni.spell.info(42930)},
- DeepFreeze = {id = 44572, name = ni.spell.info(44572)},
- FrostArmor = {id = 7301, name = ni.spell.info(7301)},
- FrostNova = {id = 42917, name = ni.spell.info(42917)},
- FrostWard = {id = 32796, name = ni.spell.info(32796)},
- Frostbolt = {id = 42841, name = ni.spell.info(42841)},
- IceArmor = {id = 27124, name = ni.spell.info(27124)},
- IceBarrier = {id = 43038, name = ni.spell.info(43038)},
- IceBlock = {id = 45438, name = ni.spell.info(45438)},
- IceLance = {id = 42913, name = ni.spell.info(42913)},
- IcyVeins = {id = 12472, name = ni.spell.info(12472)},
- SummonWaterElemental = {id = 31687, name = ni.spell.info(31687)},
+Blizzard = {id = 42939, name = ni.spell.info(42939)},
+ColdSnap = {id = 11958, name = ni.spell.info(11958)},
+ConeofCold = {id = 42930, name = ni.spell.info(42930)},
+DeepFreeze = {id = 44572, name = ni.spell.info(44572)},
+FrostArmor = {id = 7301, name = ni.spell.info(7301)},
+FrostNova = {id = 42917, name = ni.spell.info(42917)},
+FrostWard = {id = 32796, name = ni.spell.info(32796)},
+Frostbolt = {id = 42841, name = ni.spell.info(42841)},
+IceArmor = {id = 27124, name = ni.spell.info(27124)},
+IceBarrier = {id = 43038, name = ni.spell.info(43038)},
+IceBlock = {id = 45438, name = ni.spell.info(45438)},
+IceLance = {id = 42914, name = ni.spell.info(42914)},
+IcyVeins = {id = 12472, name = ni.spell.info(12472)},
+SummonWaterElemental = {id = 31687, name = ni.spell.info(31687)},
 --Pet
- Waterbolt = {id = 31707, name = ni.spell.info(31707)},
+Waterbolt = {id = 31707, name = ni.spell.info(31707)},
 }
 
 local enables = {
@@ -76,7 +76,7 @@ local enables = {
    ["ArcaneIntellectGroup"] = false,
 }
 local values = {
-   ["BlizzardCount"] = 3
+   ["BlizzardCount"] = 2
 }
 local menus = {
    ["Armor"] = spells.IceArmor.name
@@ -96,9 +96,9 @@ end
 local ui = {
    settingsfile = ni.player.guid() .. "_frost_mage_wrath.json",
    callback = GUICallback,
-   {type = "label", text = "Armor Selection"},
    {
       type = "combobox",
+      text = "Armor Selection",
       menu = {
          {selected = (menus["Armor"] == spells.IceArmor.name), text = spells.IceArmor.name},
          {selected = (menus["Armor"] == spells.FrostArmor.name), text = spells.FrostArmor.name},
@@ -121,6 +121,7 @@ local ui = {
       enabled = enables["Blizzard"],
       key = "Blizzard"
    },
+   {type = "label", text = "SLIDER NOT WORKING"},
    {
       type = "slider",
       text = spells.Blizzard.name,
@@ -140,8 +141,7 @@ local ui = {
    {type = "label", text = "Frost Mage by Nuok"}
 }
 
-local t,
-   p = "target", "player"
+local t, p = "target", "player"
 
 local cache = {
    mana = 0,
